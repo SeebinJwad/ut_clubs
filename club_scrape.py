4ab3ebf53cc39056c5ded940d0f0b5136573d214
Live session user <kazijawadahmed@gmail.com>
2021-12-15 13:29:30 -0600

    reqs

--- club_scrape.py
+++ club_scrape.py
@@ -1,10 +1,18 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
 
 driver = webdriver.Chrome()
 
-driver.get("utexas.campuslab.com/engage/organizations")
+driver.get("https://utexas.campuslabs.com/engage/organizations")
 
-driver.execute_script("window.scrollTo(0, Y)")
+load_more = driver.find_element(By.XPATH, "//div[@class='outlinedButton']")
 
+while True:
+    try:
+        load_more.click()
+        time.sleep(2)
+    except:
+        break
 
 
